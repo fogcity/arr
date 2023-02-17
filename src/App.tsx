40648de04f25d05450751bdd8ddf5f876e4f4490
火云邪神 <xymzhs@gmail.com>
2023-02-17 14:57:09 +0800

    chore: update rollup to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
-
 import { piano,getIntervalRelations,getWholeTone,decreaseSignToIncreaseSign } from '../src'
 console.log(piano,getIntervalRelations('F1','G1'));
+
+
+
+function App() {
+  
+
+  
+  return (<>piano,getIntervalRelations('F1','G1')</>
+  );
+}
+
+export default App;
