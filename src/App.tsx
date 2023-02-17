120f1be3fe1e637f9d26ced0a59f321efccb5c51
火云邪神 <xymzhs@gmail.com>
2023-02-17 15:05:21 +0800

    chore: update struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { piano,getIntervalRelations,getWholeTone,decreaseSignToIncreaseSign } from '../src'
-console.log(piano,getIntervalRelations('F1','G1'));
+import { piano, getIntervalRelations, getWholeTone, decreaseSignToIncreaseSign } from '../dist';
+console.log(piano, getIntervalRelations('F1', 'G1'));
 
-
-
-function App() {
-  
-
-  
-  return (<>piano,getIntervalRelations('F1','G1')</>
-  );
-}
+function App() {}
 
 export default App;
